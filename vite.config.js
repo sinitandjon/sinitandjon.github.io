0d8784fb91a8a9cc4a36231904eea1a2c43984d6
sinitandjon yeo <sinitandjon.yeo@polynom.io>
2025-06-26 09:23:43 +0200

    Update Vite configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/sinitandjon.github.io/', // replace with your repo name
+  base: '/', // ✅ For user/organization repo named exactly "username.github.io"
+  plugins: [react()]
 })
